0d4ea24f1af3547a62bbb419f57e1c854ec62979
virinospaco <43137669+MargaritaBasalaeva@users.noreply.github.com>
2026-04-16 21:06:50 +0300

    Removed comments and some trash

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import bot_token
 from handlers import user
-# Включаем логирование, чтобы не пропустить важные сообщения
+
 logging.basicConfig(level=logging.INFO)
-# Объект бота
 bot = Bot(token=bot_token)
-# Диспетчер
 dp = Dispatcher()
 
 async def main():
